cd9f9cbbfb2f1ad6e2368e895249a68c2f5960bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-24 03:30:43 +0000

    Deploy captbaritone/webamp to github.com/captbaritone/webamp.git:gh-pages

---
+++ precache-manifest.66d162ac2cb84c7b6bddd459ca427296.js
@@ -0,0 +1,81 @@
+self.__precacheManifest = [
+  {
+    "url": "/butterchurn-initial-dependencies-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/butterchurn-initial-dependencies-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/music-metadata-browser-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/music-metadata-browser-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/milkdrop-preset-converter-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/milkdrop-preset-converter-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/jszip-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/jszip-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/butterchurn-non-minimal-presets-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/butterchurn-non-minimal-presets-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/webamp-472f53c2f773e53d41df.js"
+  },
+  {
+    "url": "/webamp-472f53c2f773e53d41df.js.map"
+  },
+  {
+    "url": "/images/manifest/icon-144x144-06bb87f22f8080f239f096e856435678.png"
+  },
+  {
+    "url": "/images/preview-42304ea56f49c60e3d2c6c97d1bdddeb.png"
+  },
+  {
+    "url": "/css/page-6266f8359de1c8eb51d3c465e7c33e94.css"
+  },
+  {
+    "url": "/images/favicon-0cc27932e7d5cbda2849b27e6e6dc3c8.ico"
+  },
+  {
+    "url": "/images/manifest/icon-48x48-67470808290acc080f2d154fd1df70d1.png"
+  },
+  {
+    "url": "/images/manifest/icon-72x72-9d9aaf3f4dad41359616e32e082dc71c.png"
+  },
+  {
+    "url": "/images/manifest/icon-96x96-44fece775401fd6184d71022778b4edd.png"
+  },
+  {
+    "url": "/images/manifest/icon-192x192-fd139d3437e0cad44d05f85b226a24d7.png"
+  },
+  {
+    "url": "/images/share/butterchurn-bg-d09543a6c6d8150469b722454c583c07.png"
+  },
+  {
+    "revision": "b789a4be0338447f6b713644890ca3ba",
+    "url": "/index.html"
+  },
+  {
+    "url": "/manifest.60fc98cc18ea0b3ab073cda74610efa1.json"
+  },
+  {
+    "url": "/images/manifest/icon_513x513.1f27cb4bba614c00bf595696efa4435d.svg"
+  },
+  {
+    "url": "/images/manifest/icon_512x512.5228b937afc3368434b594aa65be2c08.png"
+  },
+  {
+    "url": "/images/manifest/icon_192x192.ec9b715176586bf58ec5abe0354efd85.png"
+  }
+];
